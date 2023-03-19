4e38296851b66017dbcb7e6591d527dde43db883
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-19 14:39:21 +0700

    cokl

--- transfer-to-branch-results.sh
+++ transfer-to-branch-results.sh
@@ -9,7 +9,6 @@ git config --local user.name "binsarjr"
 git branch -m main
 git add results
 git commit -m "results folder"
-git stash save --keep-index
 
 git checkout -b results
 # Use the git merge --no-commit <source-branch> command to merge the changes from the source branch (the branch that currently has the folder) into the target branch. The --no-commit option prevents Git from automatically committing the changes, which will allow you to make additional changes before committing.
@@ -17,4 +16,5 @@ git checkout -b results
 # # Use the git reset HEAD <folder-name> command to unstage the folder changes.
 # git reset HEAD results
 # # Use the git checkout -- <folder-name> command to overwrite the folder changes in the target branch with the version from the source branch.
-# git checkout -- results
+git reset HEAD --hard
+git checkout -- results
